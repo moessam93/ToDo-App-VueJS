d5569a92a41345e7b405f1cf72e72cc2ad4c15fc
Mohamed Essam <mhran9333@gmail.com>
2019-10-05 17:16:05 +0200

    add files

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
               style="width:200%"
               id="filter-todos"
               placeholder="Search todos"
+              v-model="findTodoName"
             />
           </div>
         </div>
@@ -34,32 +35,40 @@
           class="form-control mr-5"
         />
         <input id="todo-duedate" v-model="todoDueDate" type="date" class="form-control mr-5" />
-        <button class="btn btn-dark float-right" @click="addTodo">Submit</button>
+        <button class="btn btn-dark float-right" @click.prevent="addTodo">Submit</button>
       </form>
       <div class="container">
-        <button class="btn-dark float-left" id="control-complete" value="false">Hide Completed</button>
+        <button
+          class="btn-dark float-left"
+          id="control-complete"
+          value="false"
+          @click.prevent="hideCompleted($event)"
+        >Hide Completed</button>
 
-        <select name="sort-options" id="select-sort" class="float-right">
+        <select name="sort-options" id="select-sort" class="float-right" @input="todoSort($event)">
           <option value="null">--</option>
           <option value="Sort Date">Sort Date</option>
           <option value="Sort Name">Sort Name</option>
         </select>
       </div>
       <br />
-      <ul id="todos">
-        <li v-for="(todo,index) in todosArray" :key="index">
-          <span contenteditable="true" class="editable" id="edit-name">{{todo.todoName}}</span>
-          <button class='btn btn-danger btn-sm float-right delete'
-           >🗑️</button>
-          <span id="todo-duedate-li" class="float-right">{{todo.todoDueDate}}</span>
-          <input type="date" id="edit-date" class="float-right">
-        </li>
-      </ul>
+      <div class="container">
+        <b-list-group id="todos" v-for="(todo,index) in filterTodos" :key="index">
+          <TodoItem
+            :todo="todo"
+            :index="index"
+            :completeTodo="completeTodo"
+            :editTodoName="editTodoName"
+            :deleteTodo="deleteTodo"
+            :editTodoDueDate="editTodoDueDate"
+          />
+        </b-list-group>
+      </div>
     </div>
   </div>
 </template>
-
 <script>
+import TodoItem from "./components/TodoItem.vue";
 export default {
   name: "app",
   data() {
@@ -67,15 +76,113 @@ export default {
       todoName: "",
       todoDueDate: "",
       todoStatus: false,
-      todosArray: []
+      todosArray: [],
+      findTodoName: "",
+      hideCompletedFlag: false
     };
+  },
+  components: {
+    TodoItem
+  },
+  computed: {
+    filterTodos() {
+      let text = new RegExp(this.findTodoName, "i");
+      if (!this.hideCompletedFlag)
+        return this.todosArray.filter(todo => todo.todoName.match(text));
+      else
+        return this.todosArray.filter(
+          todo => todo.todoName.match(text) && todo.todoStatus === false);
+    },
+    getTodoID(){
+        return this.todosArray.length;
+    }
+  },
+  mounted() {
+    if (JSON.parse(localStorage.getItem("Todos")) != null){
+      this.todosArray = JSON.parse(localStorage.getItem("Todos"));
+    }
   },
   methods: {
     addTodo() {
       let todoName = this.todoName;
-      let todoDueDate = this.todoDueDate;
+      let todoDueDate = this.todoDueDate
+        .split("-")
+        .reverse()
+        .join("-");
       let todoStatus = this.todoStatus;
-      this.todosArray.push({ todoName, todoDueDate, todoStatus });
+      let todoID = this.getTodoID;
+      this.todosArray.push({ todoID, todoName, todoDueDate, todoStatus });
+      this.todoName = "";
+      this.todoDueDate = "";
+      localStorage.setItem("Todos", JSON.stringify(this.todosArray));
+    },
+    deleteTodo(e, index) {
+      const ID = this.filterTodos[index].todoID;
+      const todosArrayIndex = this.todosArray.indexOf(
+        this.todosArray.find(item => item.todoID === ID)
+      );
+      this.todosArray.splice(todosArrayIndex, 1);
+      this.todosArray.forEach((todo,index)=>todo.todoID=index)
+      localStorage.setItem("Todos", JSON.stringify(this.todosArray));
+    },
+    editTodoName(e, index) {
+      e.preventDefault();
+      this.filterTodos[index].todoName = e.target.innerText;
+      e.target.blur();
+      localStorage.setItem("Todos", JSON.stringify(this.todosArray));
+    },
+    completeTodo(e,index) {
+      e.target.checked=this.filterTodos[index].todoStatus;
+      this.filterTodos[index].todoStatus =
+        this.filterTodos[index].todoStatus !== true;
+      localStorage.setItem("Todos", JSON.stringify(this.todosArray));
+    },
+    editTodoDueDate(e, index) {
+      this.filterTodos[index].todoDueDate = e.target.value
+        .split("-")
+        .reverse()
+        .join("-");
+      e.target.value=null
+      localStorage.setItem("Todos", JSON.stringify(this.todosArray));
+    },
+    hideCompleted(e) {
+      if (
+        this.hideCompletedFlag === false &&
+        this.todosArray.find(item => item.todoStatus === true)
+      ) {
+        e.target.innerText = "Show All";
+        this.hideCompletedFlag = true;
+      } else {
+        e.target.innerText = "Hide Completed";
+        this.hideCompletedFlag = false;
+      }
+    },
+    todoSort(e) {
+      if (e.target.value === "Sort Name") {
+        this.todosArray.sort((a, b) => a.todoName.localeCompare(b.todoName));
+      } else if (e.target.value === "Sort Date") {
+        this.todosArray.sort((a, b) =>
+          a.todoDueDate
+            .split("-")
+            .reverse()
+            .join("-") >
+          b.todoDueDate
+            .split("-")
+            .reverse()
+            .join("-")
+            ? 1
+            : a.todoDueDate
+                .split("-")
+                .reverse()
+                .join("-") <
+              b.todoDueDate
+                .split("-")
+                .reverse()
+                .join("-")
+            ? -1
+            : 0
+        );
+      }
     }
   }
 };
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="todo-item">
+    <b-list-group-item>
+      <input
+        id="complete"
+        type="checkbox"
+        class="float-left"
+        :checked="todo.todoStatus"
+        @change.prevent="completeTodo($event,index)"
+      />
+      <span
+        contenteditable="true"
+        class="editable"
+        id="edit-name"
+        @keydown.tab="editTodoName($event,index)"
+        @keydown.enter="editTodoName($event,index)"
+      >{{todo.todoName}}</span>
+      <b-button class="btn btn-danger btn-sm float-right delete" @click="deleteTodo($event,index)">🗑️</b-button>
+      <span id="todo-duedate-li" class="float-right">{{todo.todoDueDate}}</span>
+      <input type="date" id="edit-date" class="float-right" @keyup.enter="editTodoDueDate($event,index)" />
+    </b-list-group-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todo-item",
+  props: {
+    todo: Object,
+    editTodoName: Function,
+    deleteTodo: Function,
+    editTodoDueDate: Function,
+    completeTodo: Function,
+    index: Number
+  }
+};
+</script>
+
+<style>
+</style>
